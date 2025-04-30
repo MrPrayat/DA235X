a79e3202d08808811bcd3973db0d4388e59a0566
Pooya Rayat <rayat@kth.se>
2025-04-30 16:29:18 +0200

    GPT 4.1 fixed 2 PDFs with missing annotations

--- evaluation/evaluate_outputs.py
+++ evaluation/evaluate_outputs.py
@@ -187,7 +187,7 @@ def main():
     print(f"Total Recall: {summary['recall']:.2f}")
     print(f"Total F1 Score: {summary['f1_score']:.2f}")
 
-    log_run_to_csv(results, run_name="baseline_GPT4o_v2_3", notes="Fixed issue with 2 PDFs were missing ground truth")
+    log_run_to_csv(results, run_name="baseline_GPT4.1_v3", notes="Fixed issue with 2 PDFs were missing ground truth")
 
 
 if __name__ == "__main__":
